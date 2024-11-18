6bd2ed0e176f30399056cb4de2aeb4adba5e3d98
Muhammad Shehzad <127086042+Muhammd-Shehzad@users.noreply.github.com>
2024-11-18 22:19:47 +0500

    done model and without modle api

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:apitesting2/Ui/APIsWithModel/GetComment/get_comment.dart';
-import 'package:apitesting2/Ui/APIsWithOutModel/GetTodos/get_todos.dart';
-import 'package:apitesting2/Ui/APIsWithOutModel/GetUser/get_user.dart';
+import 'package:apitesting2/Ui/APIsWithModel/GetPhoto/get_photo.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
@@ -11,18 +10,10 @@ void main() {
       designSize: const Size(360, 390),
       splitScreenMode: true,
       minTextAdapt: true,
-      builder: (context, child) => GetMaterialApp(home: GetComment()
-          // GetTodos()
-          // GetUser() // user without model
-          // UserPost()
-          // GetComment()
-          // UserPostWithoutModel()
-          // UserPost(),
-          // GetPhoto(),
-          // HomeScreen(),
-          // Get1Post(),
-          // GetPost(),
-          ),
+      builder: (context, child) => const GetMaterialApp(
+        home: GetPhoto(),
+        // GetComment(),
+      ),
     ),
   );
 }
